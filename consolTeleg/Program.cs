05311b0207d9b6c179b8a11f84f6c7921e7e9b62
Abbas <khalilikhoo2@gmail.com>
2020-07-31 14:46:42 +0430

    init

---
+++ consolTeleg/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consolTeleg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var t = new Telegcs();
+            t.Connect();
+            Console.WriteLine("Conected OK!");
+            //t.Authentication();
+            Console.WriteLine("Authentication OK!");
+            Console.WriteLine("Enter Text to send : ");
+            var msg1 = Console.ReadLine();
+            while (msg1 != "x")
+            {
+                var msg = Console.ReadLine();
+                t.SendMessage(msg);
+                Console.WriteLine("Message has been sent.");
+                msg1 = msg;
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ consolTeleg/Telegcs.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using TeleSharp.TL;
+using TeleSharp.TL.Auth;
+using TLSharp.Core;
+using TLSharp.Core.Network;
+
+namespace consolTeleg
+{
+    public class Telegcs
+    {
+
+        private const int ApiId = 1469590000811;
+        const string ApiHash = "b0dd0731c440911a360ca15ad88b14ca";
+
+        private readonly TelegramClient _tele;
+
+        public Telegcs()
+        {
+            _tele = new TelegramClient(ApiId, ApiHash,new FileSessionStore());
+        }
+        
+        public void Connect()
+        {
+            _tele.ConnectAsync().Wait();
+        }
+
+        public async void Authentication()
+        {
+            if (!File.Exists("session.dat"))
+            {
+                var hash = await _tele.SendCodeRequestAsync("+989308110000");
+                var code = "w5070"; // you can change code in debugger
+                var user = await _tele.MakeAuthAsync("+989308110000", hash, code);
+            }
+        }
+
+        public async void SendMessage(string msg)
+        {
+            var result = await _tele.GetContactsAsync();
+
+            //find recipient in contacts
+            var user = result.users.lists
+                .Where(x => x.GetType() == typeof(TLUser))
+                .Cast<TLUser>()
+                .FirstOrDefault(x => x.phone == "989905090000");
+
+            //send message
+            await _tele.SendMessageAsync(new TLInputPeerUser() { user_id = user.id }, msg);
+        }
+
+    }
+}
